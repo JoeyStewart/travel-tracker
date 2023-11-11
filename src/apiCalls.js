304a53bb4d43c3d8616c87d337bc3851df1d4761
Joseph Stewart <JosephStewart1752@gmail.com>
2023-11-11 00:11:47 -0500

    new destinations fetch request added

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -13,3 +13,19 @@ export const fetchTripsData = () => {
         console.error("Fetch error:", err);
       });
   };
+
+  export const fetchDestinationData = () => {
+    return fetch("http://localhost:3001/api/v1/destinations")
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Network response Error");
+        } 
+        return response.json();
+      })
+      .then((data) => {
+         return data.destinations
+      })
+      .catch((err) => {
+        console.error("Fetch error:", err);
+      });
+  };
