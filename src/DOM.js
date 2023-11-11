6b64f8903c3a9166c8bb53e15c9c54f6cb62e985
Joseph Stewart <JosephStewart1752@gmail.com>
2023-11-11 18:29:10 -0500

    total money spent function skeleton created

--- src/DOM.js
+++ src/DOM.js
@@ -92,10 +92,33 @@ const renderDestinationInfo = (trips) => {
     }
 };
 
+const renderMoney = (trips) => {
+    moneyContent.classList.remove('hidden')
+    moneyContent.innerHTML = '';
+    
+    const approvedTrips = trips.filter((trip) => trip.status === 'approved');
+        approvedTrips.sort((a, b) => new Date(b.date) - new Date(a.date));
+    
+    if (approvedTrips.length > 0) {
+      const mostRecentApprovedTrip = approvedTrips[0];
+      moneyContent.innerHTML += `
+        <div class="trip-info">
+          <p>Trip ID: ${mostRecentApprovedTrip.id}</p>
+          <p>User ID: ${mostRecentApprovedTrip.userID}</p>
+          <p>Destination ID: ${mostRecentApprovedTrip.destinationID}</p>
+          <p>Date: ${mostRecentApprovedTrip.date}</p>
+          <p>Duration: ${mostRecentApprovedTrip.duration}</p>
+          <p>Status: ${mostRecentApprovedTrip.status}</p>
+          <p>Suggested Activities: ${mostRecentApprovedTrip.suggestedActivities}</p>
+        </div>`
+  }
+}
+
+
 export {
     renderApprovedTrips,
     renderPendingTrips,
     renderPastTrips,
-    renderDestinationInfo
-    // renderMoney
+    renderDestinationInfo,
+    renderMoney
 }
