11fed5e5d1168985f0f8c1090c805973dece81db
Joseph Stewart <JosephStewart1752@gmail.com>
2023-11-10 20:08:51 -0500

    Add function to display approved trips on upcoming container

---
+++ src/DOM.js
@@ -0,0 +1,24 @@
+import {fetchTripsData} from './apiCalls.js'
+
+
+const upcomingContent = document.querySelector('.upcoming-content')
+
+const renderApprovedTrips = (trips) => {
+    upcomingContent.classList.remove('hidden')
+    
+    upcomingContent.innerHTML = '';
+  
+    upcomingContent.innerHTML += `
+    <p>${trips.id}</p>
+    <p>${trips.userID}</p>
+    <p>${trips.destinationID}</p>
+    <p>${trips.date}</p>
+    <p>${trips.duration}</p>
+    <p>${trips.status}</p>
+    <p>${trips.suggestedActivites}</p>
+    `;
+  }
+
+export {
+    renderApprovedTrips
+}
