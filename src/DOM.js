01903d1a4f33f86afe8d4271bf1a51cc36674901
Joseph Stewart <JosephStewart1752@gmail.com>
2023-11-10 20:38:37 -0500

    rendered upcoming trips onto webpage'

--- src/DOM.js
+++ src/DOM.js
@@ -7,17 +7,18 @@ const renderApprovedTrips = (trips) => {
     upcomingContent.classList.remove('hidden')
     
     upcomingContent.innerHTML = '';
-  
+    trips.forEach((trip) => {
     upcomingContent.innerHTML += `
-    <p>${trips.id}</p>
-    <p>${trips.userID}</p>
-    <p>${trips.destinationID}</p>
-    <p>${trips.date}</p>
-    <p>${trips.duration}</p>
-    <p>${trips.status}</p>
-    <p>${trips.suggestedActivites}</p>
+    <p>id: ${trip.id}</p>
+    <p>userID: ${trip.userID}</p>
+    <p>destinationID: ${trip.destinationID}</p>
+    <p>date: ${trip.date}</p>
+    <p>duration: ${trip.duration}</p>
+    <p>status: ${trip.status}</p>
+    <p>activites: ${trip.suggestedActivites}</p>
     `;
-  }
+  })
+}
 
 export {
     renderApprovedTrips
