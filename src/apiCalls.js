ee7ee1f3f31366ac922f5b04eaedd3cbf6f33d3c
Joseph Stewart <JosephStewart1752@gmail.com>
2023-11-12 19:56:53 -0500

    Data gets posted to trips array

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -15,63 +15,63 @@ export const fetchTripsData = () => {
       });
   };
 
-  export const fetchDestinationData = () => {
-    return fetch("http://localhost:3001/api/v1/destinations")
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error("Network response Error");
-        } 
-        return response.json();
-      })
-      .then((data) => {
-         return data.destinations
-      })
-      .catch((err) => {
-        console.error("Fetch error:", err);
-      });
-  };
+export const fetchDestinationData = () => {
+  return fetch("http://localhost:3001/api/v1/destinations")
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Network response Error");
+      } 
+      return response.json();
+    })
+    .then((data) => {
+        return data.destinations
+    })
+    .catch((err) => {
+      console.error("Fetch error:", err);
+    });
+};
 
 // POST requests
-  export const postTripData = (tripInput) => {
-    fetch("http://localhost:3001/api/v1/trips", {
-      method: "POST",
-      body: JSON.stringify(tripInput),
-      headers: {
-        "Content-Type": "application/json",
+export const postTripData = (tripInput) => {
+  fetch("http://localhost:3001/api/v1/trips", {
+    method: "POST",
+    body: JSON.stringify(tripInput),
+    headers: {
+      "Content-Type": "application/json",
+    }
+  })
+    .then((response) => {
+      if (!response.ok && response.status !== 422) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
       }
+      return response.json();
     })
-      .then((response) => {
-        if (!response.ok && response.status !== 422) {
-          throw new Error(`HTTP error! Status: ${response.status}`);
-        }
-        return response.json();
-      })
-      .then((data) => {
-        console.log("New activity data:", data);
-      })
-      .catch((error) => {
-        console.error("Error:", error);
-      });
-  };
+    .then((data) => {
+      console.log("New activity data:", data);
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+    });
+};
 
-  export const postDestinationData = (tripInput) => {
-    fetch("http://localhost:3001/api/v1/trips", {
-      method: "POST",
-      body: JSON.stringify(tripInput),
-      headers: {
-        "Content-Type": "application/json",
+export const postDestinationData = (destInput) => {
+  fetch("http://localhost:3001/api/v1/trips", {
+    method: "POST",
+    body: JSON.stringify(destInput),
+    headers: {
+      "Content-Type": "application/json",
+    }
+  })
+    .then((response) => {
+      if (!response.ok && response.status !== 422) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
       }
+      return response.json();
     })
-      .then((response) => {
-        if (!response.ok && response.status !== 422) {
-          throw new Error(`HTTP error! Status: ${response.status}`);
-        }
-        return response.json();
-      })
-      .then((data) => {
-        console.log("New activity data:", data);
-      })
-      .catch((error) => {
-        console.error("Error:", error);
-      });
-  };
+    .then((data) => {
+      console.log("New activity data:", data);
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+    });
+};
