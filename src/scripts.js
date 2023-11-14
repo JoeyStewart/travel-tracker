6de4bea856eaa9b53fe451a49647cb6893def056
Joseph Stewart <JosephStewart1752@gmail.com>
2023-11-14 12:24:00 -0500

    Destination now added to past trip

--- src/scripts.js
+++ src/scripts.js
@@ -16,7 +16,7 @@ export let userID
 
 let approvedTripData
 let pendingTripData
-let pastTripData
+let pastTripData = {}
 let moneySpent = {}
 let destinationInfo
 
@@ -33,7 +33,6 @@ submitButton.addEventListener("click", () => {
     status: "pending",
     suggestedActivities: [],
     }
-
   if (
     !dateInput.value || 
     !destinationInput.value || 
@@ -42,7 +41,15 @@ submitButton.addEventListener("click", () => {
    {
     alert("You must fill all information fields correctly before submitting");
   } else {
-    postTripData(tripObject);
+    // postTripData(tripObject)
+    // .then(() => {
+    //   fetchTripsData()
+    //     .then((tripData) => {
+    //       trips = tripData;
+    //       renderPendingTrips(trips, userID);
+    //       hideElement(tripForm);
+    //     });
+    // });
   }
     dateInput.value = '' 
     destinationInput.value = ''
@@ -55,9 +62,10 @@ window.addEventListener('load', () => {
       .then((data) => {
         approvedTripData = data[0];
         pendingTripData = data[0];
-        pastTripData = data[0];
-        moneySpent["trips"] = data[0]
-        moneySpent["destinations"] = data[1]
+        pastTripData["trips"] = data[0];
+        pastTripData["destinations"] = data[1];
+        moneySpent["trips"] = data[0];
+        moneySpent["destinations"] = data[1];
         destinationInfo = data[1];
       })
       .catch((error) => {
@@ -93,7 +101,9 @@ const populatePending = (data, userID) => {
 }
 
 const populatePast = (data, userID) => {
-  renderPastTrips(data, userID)
+  const tripsData = data.trips;
+  const destinationData = data.destinations
+  renderPastTrips(tripsData, destinationData, userID)
 }
 
 const populateMoney = (data, userID) => {
